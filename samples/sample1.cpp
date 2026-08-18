523685c01b2745e8354846bc59c3620acefe711a
Sutirtho Banerjee <sutirthobanerjee2508@gmail.com>
2026-08-18 20:48:22 +0530

    Implement operations in foo function using dyn_var
    Added various operations on dyn_var<int> in foo function.

--- samples/sample1.cpp
+++ samples/sample1.cpp
@@ -3,7 +3,38 @@
 #include "builder/dyn_var.h"
 #include <iostream>
 using builder::dyn_var;
+static void foo(void){
+	dyn_var<int> a = 0;
+	dyn_var<int> b = a;
+	a && b;
+	a & b;
+	b+1;
+	1+b;
+	a%2;
+	a | 2;
+	a >> 2;
+	b<<3;
+	a^b;
+	a+=b;
+	a*=b;
+	a&=b;
+	a|=b;
+	a^=b;
+	~b;
+	&b;
+}
 
+int main(int argc, char*argv[](
+	builder :: builder_context context;
+	auto ast = context.extract_ast_from_function(foo);
+	ast->dump(std::cout,0);
+	
+
+
+
+
+
+)
 // A simple straight line code with 2 variable declarations and one operator
 static void foo(void) {
 	dyn_var<int> a = 0;
